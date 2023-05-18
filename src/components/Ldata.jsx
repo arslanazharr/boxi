e92474efe9cdb702bc687d6a48bfc8ae4ae5c4c4
Arslan <arslanazhar00@gmail.com>
2023-05-19 00:28:44 +0500

    second-commit

---
+++ src/components/Ldata.jsx
@@ -0,0 +1,33 @@
+import campaigns from "../graphics/sidebar icons/campaigns.png";
+import drive from "../graphics/sidebar icons/drive.png";
+import list from "../graphics/sidebar icons/list.png";
+import production from "../graphics/sidebar icons/production.png";
+import settings from "../graphics/sidebar icons/settings.png";
+import team from "../graphics/sidebar icons/team.png";
+
+const Ldata = [
+  {
+    imgsrc: campaigns,
+    title: "Software Development",
+  },
+  {
+    imgsrc: drive,
+    title: "Drive",
+  },
+  {
+    imgsrc: team,
+    title: "Boxi Team",
+  },
+  {
+    imgsrc: production,
+    title: "Production",
+  },
+  {
+    imgsrc: list,
+    title: "Fix List",
+  },
+  {
+    imgsrc: settings,
+    title: "Settings",
+  },
+];
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,43 +1,38 @@
 import { Fragment, useState } from "react";
 import { Dialog, Menu, Transition } from "@headlessui/react";
-import {
-  Bars3Icon,
-  BellIcon,
-  CalendarIcon,
-  ChartPieIcon,
-  Cog6ToothIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import {
   ChevronDownIcon,
   MagnifyingGlassIcon,
 } from "@heroicons/react/20/solid";
 
-// logo
+import campaigns from "../graphics/sidebar icons/campaigns.png";
+import drive from "../graphics/sidebar icons/drive.png";
+import list from "../graphics/sidebar icons/list.png";
+import production from "../graphics/sidebar icons/production.png";
+import settings from "../graphics/sidebar icons/settings.png";
+import team from "../graphics/sidebar icons/team.png";
+import greenArrow from "../graphics/sidebar icons/greenArrow.png";
+import whiteArrow from "../graphics/sidebar icons/whiteArrow.png";
 import logo from "../graphics/logo.png";
-// logo
 
-// css
-import "../components/sidebar.css";
-// css
+import "./sidebar.css";
 
-const navigation = [
-  { name: "Campaigns", href: "#", icon: HomeIcon, current: true },
-  { name: "Team", href: "#", icon: UsersIcon, current: false },
-  { name: "Projects", href: "#", icon: FolderIcon, current: false },
-  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
-  { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
-  { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
+const Ldata = [
+  {
+    name: "Campaigns",
+    href: "#",
+    icon: campaigns,
+    arrow: greenArrow,
+    current: true,
+  },
+  { name: "Drive", href: "#", icon: drive, arrow: whiteArrow },
+  { name: "Boxi Team", href: "#", icon: team },
+  { name: "Production", href: "#", icon: production },
+  { name: "Fix List", href: "#", icon: list },
+  { name: "Settings", href: "#", icon: settings },
 ];
-const teams = [
-  { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
-  { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
-  { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
-];
+
 const userNavigation = [
   { name: "Your profile", href: "#" },
   { name: "Sign out", href: "#" },
@@ -47,7 +42,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function SideBar() {
+export default function Example() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
@@ -106,7 +101,7 @@ export default function SideBar() {
                     </div>
                   </Transition.Child>
                   {/* Sidebar component, swap this element with another sidebar if you like */}
-                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
+                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-green-600 main-div">
                     <div className="flex h-16 shrink-0 items-center">
                       <img
                         className="h-8 w-auto logo"
@@ -117,69 +112,36 @@ export default function SideBar() {
                     <nav className="flex flex-1 flex-col">
                       <ul role="list" className="flex flex-1 flex-col gap-y-7">
                         <li>
-                          <ul role="list" className="-mx-2 space-y-1">
-                            {navigation.map((item) => (
-                              <li key={item.name}>
+                          <ul role="list" className="-mx-2 space-y-2">
+                            {Ldata.map((item, index) => (
+                              <li
+                                key={item.name}
+                                className={index < 1 ? "custom-class" : ""}
+                              >
                                 <a
                                   href={item.href}
                                   className={classNames(
                                     item.current
-                                      ? "bg-indigo-700 text-white"
-                                      : "text-indigo-200 hover:text-white hover:bg-indigo-700",
-                                    "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                                      ? "bg-white text-green-600"
+                                      : "text-white",
+                                    "group flex gap-x-4 rounded-md p-2 text-sm leading-6 font-semibold"
                                   )}
                                 >
-                                  <item.icon
-                                    className={classNames(
-                                      item.current
-                                        ? "text-white"
-                                        : "text-indigo-200 group-hover:text-white",
-                                      "h-6 w-6 shrink-0"
-                                    )}
-                                    aria-hidden="true"
+                                  <img
+                                    className="list-icons"
+                                    src={item.icon}
+                                    alt=""
                                   />
                                   {item.name}
+                                  <img
+                                    className="arrow"
+                                    src={item.arrow}
+                                    alt=""
+                                  />
                                 </a>
                               </li>
                             ))}
                           </ul>
-                        </li>
-                        <li>
-                          <div className="text-xs font-semibold leading-6 text-indigo-200">
-                            Your teams
-                          </div>
-                          <ul role="list" className="-mx-2 mt-2 space-y-1">
-                            {teams.map((team) => (
-                              <li key={team.name}>
-                                <a
-                                  href={team.href}
-                                  className={classNames(
-                                    team.current
-                                      ? "bg-indigo-700 text-white"
-                                      : "text-indigo-200 hover:text-white hover:bg-indigo-700",
-                                    "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                                  )}
-                                >
-                                  <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">
-                                    {team.initial}
-                                  </span>
-                                  <span className="truncate">{team.name}</span>
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
-                        <li className="mt-auto">
-                          <a
-                            href="#"
-                            className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-indigo-200 hover:bg-indigo-700 hover:text-white"
-                          >
-                            <Cog6ToothIcon
-                              className="h-6 w-6 shrink-0 text-indigo-200 group-hover:text-white"
-                              aria-hidden="true"
-                            />
-                            Settings
-                          </a>
                         </li>
                       </ul>
                     </nav>
@@ -191,76 +153,36 @@ export default function SideBar() {
         </Transition.Root>
 
         {/* Static sidebar for desktop */}
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-60 lg:flex-col">
-          {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
-            <img className="logo" src={logo} alt="" />
+        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-60 lg:flex-col ">
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-green-600 main-div">
+            <div className="flex h-16 shrink-0 items-center">
+              <img className="h-8 w-auto logo" src={logo} alt="Your Company" />
+            </div>
             <nav className="flex flex-1 flex-col">
               <ul role="list" className="flex flex-1 flex-col gap-y-7">
                 <li>
-                  <ul role="list" className="-mx-2 space-y-1">
-                    {navigation.map((item) => (
-                      <li key={item.name}>
+                  <ul role="list" className="-mx-2 space-y-2">
+                    {Ldata.map((item, index) => (
+                      <li
+                        key={item.name}
+                        className={index < 1 ? "custom-class" : ""}
+                      >
                         <a
                           href={item.href}
                           className={classNames(
                             item.current
-                              ? "bg-indigo-700 text-white"
-                              : "text-indigo-200 hover:text-white hover:bg-indigo-700",
-                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                              ? "bg-white text-green-600"
+                              : "text-white",
+                            "group flex gap-x-4 rounded-md p-2 text-sm leading-6 font-semibold"
                           )}
                         >
-                          <item.icon
-                            className={classNames(
-                              item.current
-                                ? "text-white"
-                                : "text-indigo-200 group-hover:text-white",
-                              "h-6 w-6 shrink-0"
-                            )}
-                            aria-hidden="true"
-                          />
+                          <img className="list-icons" src={item.icon} alt="" />
                           {item.name}
+                          <img className="arrow" src={item.arrow} alt="" />
                         </a>
                       </li>
                     ))}
                   </ul>
-                </li>
-                <li>
-                  <div className="text-xs font-semibold leading-6 text-indigo-200">
-                    Your teams
-                  </div>
-                  <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                      <li key={team.name}>
-                        <a
-                          href={team.href}
-                          className={classNames(
-                            team.current
-                              ? "bg-indigo-700 text-white"
-                              : "text-indigo-200 hover:text-white hover:bg-indigo-700",
-                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                          )}
-                        >
-                          <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">
-                            {team.initial}
-                          </span>
-                          <span className="truncate">{team.name}</span>
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-                <li className="mt-auto">
-                  <a
-                    href="#"
-                    className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-indigo-200 hover:bg-indigo-700 hover:text-white"
-                  >
-                    <Cog6ToothIcon
-                      className="h-6 w-6 shrink-0 text-indigo-200 group-hover:text-white"
-                      aria-hidden="true"
-                    />
-                    Settings
-                  </a>
                 </li>
               </ul>
             </nav>
@@ -309,14 +231,12 @@ export default function SideBar() {
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
-
                 {/* Separator */}
                 <div
                   className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
                   aria-hidden="true"
                 />
 
-                {/* Profile dropdown */}
                 <Menu as="div" className="relative">
                   <Menu.Button className="-m-1.5 flex items-center p-1.5">
                     <span className="sr-only">Open user menu</span>
@@ -371,7 +291,7 @@ export default function SideBar() {
           </div>
 
           <main className="py-10">
-            <div className="px-4 sm:px-6 lg:px-8">{/* Your content */}</div>
+            <div className="px-4 sm:px-6 lg:px-8"></div>
           </main>
         </div>
       </div>

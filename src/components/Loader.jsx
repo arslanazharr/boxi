0bd75ff4d033a44d5a738b8d5373770b3c42fc16
Arslan <arslanazhar00@gmail.com>
2023-05-26 01:30:50 +0500

    11th commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,15 @@
+import "./loader.css";
+
+import React from "react";
+
+const Loader = () => {
+  return (
+    <>
+      <div className="loaderDiv">
+        <div class="custom-loader"></div>;
+      </div>
+    </>
+  );
+};
+
+export default Loader;

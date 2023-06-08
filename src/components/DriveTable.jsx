31d0b9ea8b460cc7123aa6e412e45c37b9427278
Arslan Azhar <aazhar@cinnova.com>
2023-06-08 10:16:38 +0500

    added sorting

--- src/components/DriveTable.jsx
+++ src/components/DriveTable.jsx
@@ -10,42 +10,49 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const DriveTable = () => {
-  const DriveTable = useSelector((state) => state.allData.driveTable);
+  const driveTableData = useSelector((state) => state.allData.driveTable);
   const dispatch = useDispatch();
 
   const [pageNumber, setPageNumber] = useState(1);
+  const [sortOrder, setSortOrder] = useState("asc");
 
   const limit = 10;
   const pageVisited = (pageNumber - 1) * limit;
+  const pageCount = Math.ceil(driveTableData.length / limit);
 
   useEffect(() => {
     dispatch(fetchDriveTable(pageNumber, limit));
   }, [pageNumber, limit]);
 
-  const displayData = DriveTable.slice(pageVisited, pageVisited + limit).map(
-    (data) => {
-      return (
-        <tr key={data.id} className="border-b">
-          <td className="py-2 pl-2 pr-4 sm:table-cell">{data.id}</td>
-          <td className="py-2 pl-2 pr-4">{data.Vendor}</td>
-          <td className="py-2 pl-2 pr-4">{data.TruckName}</td>
-          <td className="py-2 pl-2 pr-4">{data.Campaign}</td>
-          <td className="py-2 pl-2 pr-4 sm:table-cell">{data.StartDate}</td>
-          <td className="py-2 pl-2 pr-4 sm:table-cell pmtSchedule">
-            {data.PaymentSchedule}
-          </td>
-        </tr>
-      );
-    }
-  );
+  const handleSort = () => {
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
 
-  const pageCount = Math.ceil(DriveTable.length / limit);
+  const sortedData = driveTableData
+    .slice(pageVisited, pageVisited + limit)
+    .sort((a, b) => (sortOrder === "asc" ? a.id - b.id : b.id - a.id));
+
+  const displayData = sortedData.map((data) => {
+    return (
+      <tr key={data.id} className="border-b">
+        <td className="py-2 pl-2 pr-4 sm:table-cell">{data.id}</td>
+        <td className="py-2 pl-2 pr-4">{data.Vendor}</td>
+        <td className="py-2 pl-2 pr-4">{data.TruckName}</td>
+        <td className="py-2 pl-2 pr-4">{data.Campaign}</td>
+        <td className="py-2 pl-2 pr-4 sm:table-cell">{data.StartDate}</td>
+        <td className="py-2 pl-2 pr-4 sm:table-cell pmtSchedule">
+          {data.PaymentSchedule}
+        </td>
+      </tr>
+    );
+  });
 
   const increasePage = () => {
-    if (pageNumber < 10) {
+    if (pageNumber < pageCount) {
       setPageNumber(pageNumber + 1);
     }
   };
+
   const decreasePage = () => {
     if (pageNumber > 1) {
       setPageNumber(pageNumber - 1);
@@ -82,7 +89,12 @@ const DriveTable = () => {
         <table className="table w-full">
           <thead>
             <tr>
-              <th className="py-2 pl-2 pr-4 sm:table-cell">#</th>
+              <th
+                className="py-2 pl-2 pr-4 sm:table-cell sortable"
+                onClick={handleSort}
+              >
+                # {sortOrder === "asc" ? <span>▲</span> : <span>▼</span>}
+              </th>
               <th className="py-2 pl-2 pr-4">Vendor</th>
               <th className="py-2 pl-2 pr-4">Truck Name</th>
               <th className="py-2 pl-2 pr-4">Campaign</th>
@@ -96,21 +108,25 @@ const DriveTable = () => {
         <div className="flex justify-end mt-5 align-middle">
           <div className="flex space-x-2">
             <div className="buttonLeft">
-              <button className="p-2 rounded-full bg-green-300 hover:bg-green-500 transition-all ">
+              <button
+                className="p-2 w-7 h-7 rounded-full bg-green-300 hover:bg-green-500 transition-all flex items-center"
+                onClick={decreasePage}
+              >
                 <FontAwesomeIcon
                   icon={faArrowLeft}
-                  className="w-6 text-gray-500 arrowLeft"
-                  onClick={decreasePage}
+                  className="w-3 text-gray-500 arrowLeft"
                 />
               </button>
             </div>
-            <div className="flex items-center">{`${pageNumber} - ${pageCount}`}</div>
+            <div className="flex items-center text-sm">{`${pageNumber} - ${pageCount}`}</div>
             <div className="buttonRight">
-              <button className="p-2 rounded-full bg-green-300 hover:bg-green-500 transition-all ">
+              <button
+                className="p-2 w-7 h-7 rounded-full bg-green-300 hover:bg-green-500 transition-all flex items-center"
+                onClick={increasePage}
+              >
                 <FontAwesomeIcon
                   icon={faArrowRight}
-                  className="w-6 text-gray-500 arrowRight"
-                  onClick={increasePage}
+                  className="w-3 text-gray-500 arrowRight"
                 />
               </button>
             </div>
